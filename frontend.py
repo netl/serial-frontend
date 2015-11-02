86a85463081e992eddcd3fdc29174271aeddb679
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2015-11-02 13:15:18 +0000

    minor tweaks

--- frontend.py
+++ frontend.py
@@ -26,7 +26,7 @@ def clear_buttons(event):
 
 def graph_results():
 	for x in xrange(0,40):
-		canv.coords(canv.graph[x],x*12,200-x,(x+1)*12,200-(x+1))
+		canv.coords(canv.graph[x],x*12,350-x,(x+1)*12,350-(x+1))
 
 def read_serial():
 	while ser.inWaiting():
@@ -56,13 +56,13 @@ buttonStart = canv.create_image(45, 549, image=image_start, anchor='nw')
 buttonTray = canv.create_image(45, 670, image=image_tray, anchor='nw')
 
 #draw graphs
-canv.graph = [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1]
+canv.graph = range(41)
 for x in xrange(0,40):
-	canv.graph[x] = canv.create_line(x*12,200,(x+1)*12,200,fill="green",width=3)
+	canv.graph[x] = canv.create_line(x*12,350,(x+1)*12,350,fill="green",width=3)
 
 #bind events
-canv.end = Button(root, text="quit", command=quit)
-canv.end.place(x=15, y=15, width=30, height=30)
+#canv.end = Button(root, text="quit", command=quit)
+#canv.end.place(x=15, y=15, width=30, height=30)
 canv.tag_bind(buttonStart, '<ButtonPress-1>', cmd_start)
 canv.tag_bind(buttonTray, '<ButtonPress-1>', cmd_tray)
 canv.tag_bind(buttonStart, '<ButtonRelease-1>', clear_buttons)
